644994e435b08c7000c6f409ad18897c8037d6b5
Audrey <audreylh@gmail.com>
2015-01-11 10:21:01 +0800

    add select options for security field

--- public/js/form.js
+++ public/js/form.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
+  // Network
   var networks = new Bloodhound({
     datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
       queryTokenizer: Bloodhound.tokenizers.whitespace,
       limit: 10,
+      // remote lists all available networks
+      // change this to prefetch if drop down list is to match only characters entered
       remote: {
         url: '/network',
         filter: function(list) {
@@ -10,12 +13,36 @@ $(document).ready(function() {
         }
       }
   });
-
   networks.initialize();
 
-  $('.typeahead').typeahead(null, {
+  $('#prefetch .typeahead').typeahead(null, {
     name: 'networks',
     displayKey: 'name',
     source: networks.ttAdapter()
   }); 
+
+  // Security
+  var security = ["None", "WEP", "WPA", "WPA2"];
+  var substringMatcher = function(strs) {
+    return function findMatches(q, cb) {
+      matches = [];
+                
+      $.each(strs, function(i, str) {
+          matches.push({ value: str });
+      });
+
+      cb(matches);
+    };
+  };
+
+  $('#the-basics .typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  },
+  {
+    name: 'security',
+    displayKey: 'value',
+    source: substringMatcher(security)
+  });
 });
